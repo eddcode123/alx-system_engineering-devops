3fe41fdaf35e3f7c3e86074fb4239ca2a176cda6
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-03-19 20:40:20 +0300

    this commit message is for a py script that uses GET request to query a api for information

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,13 +3,12 @@
 import requests
 import sys
 
-
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(f"{url}users/{sys.argv[1]}").json()
-    todos = requests.get(f"{url}todos", params={"userId": sys.argv[1]}).json()
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
     completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print(f"Employee {user.get('name')} is done with tasks", end='')
-    print(f'({len(completed)}/{len(todos)}):')
-    [print(f"\t{task}") for task in completed]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
