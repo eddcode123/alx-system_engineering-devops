f1ead41c82765acb2b128cfa9b69bddffc89b021
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-03-19 20:16:56 +0300

    this commit message is for a py script that uses GET request to query a api for information

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" This python script uses request to retrieve data from api """
+import requests
+import sys
+
+
+def get_todo():
+    """ function uses get request to get info """
+    # get user_data
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = f'/users/{sys.argv[1]}'
+    req = requests.get(f'{url}' + f'{user}')
+    user_data = req.json()
+    print(f'Employee {user_data.get("name")} is done with tasks ', end='')
+
+    # get todos
+    todos = f'/todos/{sys.argv[1]}'
+    req_1 = requests.get(f'{url}' + f'{todos}')
+    tasks = req_1.json()
+    completed_tasks = [task for task in tasks if task.get('complete') is True]
+    print(f'({len(completed_tasks)}/{len(tasks)}):')
+    for task in completed_tasks:
+        print(f'\t{task.get('title')}')
