6ee265bfc95ecbdd6148b8913f4ba732f8a39fff
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-03-19 20:34:45 +0300

    this commit message is for a py script that uses GET request to query a api for information

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
-""" This python script uses request to retrieve data from api """
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(f"{url}users/{sys.argv[1]}").json()
+    todos = requests.get(f"{url}todos", params={"userId": sys.argv[1]}).json()
 
-if __name__ == '__main__':
-    """ function uses get request to get info """
-    # get user_data
-    url = 'https://jsonplaceholder.typicode.com/'
-    user = f'/users/{sys.argv[1]}'
-    req = requests.get(f'{url}' + f'{user}')
-    user_data = req.json()
-    print(f'Employee {user_data.get("name")} is done with tasks ', end='')
-
-    # get todos
-    todos = f'/todos/{sys.argv[1]}'
-    req_1 = requests.get(f'{url}' + f'{todos}')
-    tasks = req_1.json()
-    completed_tasks = [task.get('title') for task in tasks if task.get('complete') is True]
-    print(f'({len(completed_tasks)}/{len(tasks)}):')
-    for task in completed_tasks:
-        print(f"\t{task.get('title')}")
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print(f"Employee {user.get('name')} is done with tasks", end='')
+    print(f'({len(completed)}/{len(todos)}):')
+    [print(f"\t{task}") for task in completed]
