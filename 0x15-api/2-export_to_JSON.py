f7bfa9cad906b6c1b073a57e73d5faaa55857ee0
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-03-20 14:42:32 +0300

    this commit message is for a py script that gets todos of a user from an api and exports the values to a .json file

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Exports to-do list of user in a specified foramt to json file """
+import requests
+import sys
+import json
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": id}).json()
+
+    file_json = {
+        f'{id}': [
+            {
+                'task': f"{t.get('title')}",
+                'completed': f"{t.get('completed')}",
+                'username': f"{username}"
+            }
+            for t in todos
+        ]
+    }
+
+    with open(f'{id}.json', 'w') as json_file:
+        json.dump(file_json, json_file)
