fbbced25bb067380c484c7245e542ee04d37e567
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-03-25 11:43:03 +0300

    This commit message is for a function that queries the Reddit API and returns the number of subscribers for a given subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """uses reddit api to find numbers of subs """
-    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    url = f'http://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'My User Agent 1.0'}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
